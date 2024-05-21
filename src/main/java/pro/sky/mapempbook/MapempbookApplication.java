b5e654c68aa878cf4222ca77f8943a0e64283dc8
pvnick <pvnick@yandex.ru>
2024-05-21 19:05:46 +0700

    Initial commit

---
+++ src/main/java/pro/sky/mapempbook/MapempbookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.mapempbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapempbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapempbookApplication.class, args);
+	}
+
+}
